d35834570b1333ff52f1b22852750569449cd676
Hongji Kim <redkim94@hanmail.net>
2020-05-11 19:44:30 +0900

    updates

---
+++ hb/todo_music.m
@@ -0,0 +1,13 @@
+% TO DO
+
+
+% music
+% prepare trial sequence for each subject
+
+
+% git status: 현재 상태 (업데이트 할게 있는지 없는지)
+
+% update: 
+% 1) git add .
+% 2) git commit -m "updates"
+% 3) git push
